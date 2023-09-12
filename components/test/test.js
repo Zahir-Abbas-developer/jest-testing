6df4bba52d0d9152c5cfa5eb34d369dee7b9547a
zahir <zahir.abbas@ceative.co.uk>
2023-09-12 03:26:51 -0700

    #comment first jest

---
+++ components/test/test.js
@@ -0,0 +1,30 @@
+const Test=()=>{
+return(
+    <>
+    <div>
+    <h1>
+        heading1
+    </h1>
+    <h1>
+        heading2
+    </h1>
+    <h1>
+        heading3
+    </h1>
+    <h1>
+        heading4
+    </h1>
+    <h1>
+        heading5
+    </h1>
+    <h1>
+        heading6
+    </h1>
+    <h1>
+        heading7
+    </h1>
+    </div>
+    </>
+)
+}
+export default Test
